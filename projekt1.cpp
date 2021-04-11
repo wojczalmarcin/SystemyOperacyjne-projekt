55e9fe0622ee34eb69fa3635fd536bc3127df57b
wojczalmarcin <wojczal.m@gmail.com>
2021-04-11 15:27:09 +0200

    Dodanie drugiego etapu projektu, polegającego na zmodyfikowaniu programu tak, aby nie mogły wjechać na prawą krawędź, gdy znajduje się na niej auto jadące z mniejszą prędkością. Dodano również wyświetlanie w terminalu toru i samochodów za pomocą kolorów

--- projekt1.cpp
+++ projekt1.cpp
@@ -4,13 +4,20 @@
 #include <ncurses.h>
 #include <vector>
 #include <ctime>
+#include <mutex>
 
 using namespace std;
 
+#define ROAD_PAIR           1
+#define EDGE_PAIR           2
+#define CAR_PAIR            3
+#define GRASS_PAIR          4
+#define SPEED_MULTIPLIER    0.8
+
 // zmienna określająca długość poziomego odcinka
-static int horizontalRoadLenght = 80;
+static int horizontalRoadLenght = 40;
 // zmienna okreslajaca długość pionowego odcinka
-static int verticalRoadLenght = 20;
+static int verticalRoadLenght = 30;
 // procent prędkości na poziomym odcinku
 static float horizontalSpeedBonus = 1.4;
 // procent prędkości na pionowym odcinku
@@ -19,6 +26,8 @@ static float verticalSpeedBonus = 0.8;
 static int maxLaps = 3;
 // zmienna określająca czy program ma działać
 bool isWorking = true;
+// mutex
+mutex edgeMutex;
 
 ///
 /// Klasa opisująca samochód
@@ -42,10 +51,15 @@ class Car
     bool isDriving;
     // znak oznaczający pojazd
     int id;
+    // wektor samochodów znajdujących się na prawej krawędzi
+    vector<Car*> &carsOnRightEdgeVector;
+    // zmienna określająca, czy samochód znajduje się na prawej krawędzi
+    bool isOnRightEdge = false;
 
     public:
     // konstruktor
-    Car(int x, int y, int Id)
+    Car(int x, int y, int Id, vector<Car*> &cars) 
+        : carsOnRightEdgeVector(cars)
     {
         posX = x;
         posY = y;
@@ -80,7 +94,7 @@ class Car
         if(posX>0 && posX<horizontalRoadLenght)
         {
             // wątek jest usypiany na czas zależny od prędkości samochodu
-            this_thread::sleep_for(chrono::milliseconds((int)(3000/(speed*horizontalSpeedBonus))));
+            this_thread::sleep_for(chrono::milliseconds((int)(3000/(speed*horizontalSpeedBonus*SPEED_MULTIPLIER))));
             if(posY == 0)
                 posX-=1;
             else
@@ -89,7 +103,7 @@ class Car
         else if(posX<=0)
         {
              // wątek jest usypiany na czas zależny od prędkości samochodu
-            this_thread::sleep_for(chrono::milliseconds((int)(3000/(speed*verticalSpeedBonus))));
+            this_thread::sleep_for(chrono::milliseconds((int)(3000/(speed*verticalSpeedBonus*SPEED_MULTIPLIER))));
             if(posY>=verticalRoadLenght)
             {
                 posY = verticalRoadLenght;
@@ -101,7 +115,7 @@ class Car
         else
         {
             // wątek jest usypiany na czas zależny od prędkości samochodu
-            this_thread::sleep_for(chrono::milliseconds((int)(3000/(speed*verticalSpeedBonus))));
+            this_thread::sleep_for(chrono::milliseconds((int)(3000/(speed*verticalSpeedBonus*SPEED_MULTIPLIER))));
             if(posY<=0)
             {
                 posY = 0;
@@ -112,18 +126,57 @@ class Car
         }
     }
 
-    // funkcja poruszająca samochód do momentu aż przejdzie 3 okrążenia
-    // argumentem funkcji jest const bool w celu zapewnienia, że zmienna
-    // nie będzie modyfikowana i nie nastąpi "data race"
+    // funkcja porusza samochód do momentu aż przejdzie 3 okrążenia
+    // dodaje również samochód do wektora prawej krawędzi jeśli
+    // faktycznie się na niej znajdzie
     void driveLaps(){
+
+        bool mustWait = false;
+
         while(lap<maxLaps && isWorking == true)
         {
-            drive();
+            // Dodawanie samochodu do wektora prawej krawędzi
+            edgeMutex.lock();
+            if(posX==horizontalRoadLenght)
+            {
+                if(!isOnRightEdge)
+                {
+                    carsOnRightEdgeVector.push_back(this);
+                    isOnRightEdge = true;
+                }   
+            }
+            else if(isOnRightEdge)
+            {
+                for (int i = 0;i<carsOnRightEdgeVector.size();i++)
+                {
+                    if(carsOnRightEdgeVector[i]->getId() == id)
+                        carsOnRightEdgeVector.erase(carsOnRightEdgeVector.begin() + i);
+                }
+                isOnRightEdge = false;
+            }
+            edgeMutex.unlock();
+
+            // porównanie prędkości samochodów znajdujących się na prawej krawędzi
+            mustWait = false;
+            if(!isOnRightEdge && posX>=(horizontalRoadLenght-1) && posY>=(verticalRoadLenght-1))
+            {
+                if(carsOnRightEdgeVector.size()!=0)
+                {
+                    for(int i = 0; i < carsOnRightEdgeVector.size();i++)
+                    {
+                        if(carsOnRightEdgeVector[i]->getSpeed() < speed)
+                            mustWait = true;
+                    }
+                }
+            }
             if(posY==startPosY && posX==startPosX)
             {
                  lap++;
                  speed = rand()%60 + 40;
             }
+
+            if(!mustWait)
+                drive();
         };
         isDriving = false;
         delete this;
@@ -139,50 +192,48 @@ void draw(vector<Car*> &cars)
     nodelay(stdscr, TRUE);
     scrollok(stdscr, TRUE);
     curs_set(0);
-
+    start_color();
+    init_pair(ROAD_PAIR, COLOR_WHITE, COLOR_WHITE);
+    init_pair(EDGE_PAIR, COLOR_YELLOW, COLOR_YELLOW);
+    init_pair(CAR_PAIR, COLOR_WHITE, COLOR_RED);
+    init_pair(GRASS_PAIR, COLOR_GREEN, COLOR_GREEN);
     //jeśli użytkownik nie zakończył programu i wszystkie auta nie przejechały 3 okrążań to program trwa
     while(isWorking)
     {  
         // czyszczenie okna
         erase();
-        mvprintw(0,0,"Nacisnij dowolny przycisk, aby zakonczyc program lub poczekaj az wszystkie auta przejada 3 okrazenia");
+        mvprintw(0,0,"Nacisnij dowolny przycisk, aby zakonczyc program");
         // petle wyswietlajace tor
-        for(int i = 1; i <= horizontalRoadLenght+5;i++)
+        for(int i = 0; i <= horizontalRoadLenght*2+9;i++)
         {
-            mvprintw(1,i,"_");
-            mvprintw(verticalRoadLenght+4,i,"_");
-            if(i>7 && i <horizontalRoadLenght-1)
+            for(int j = 1;j<=verticalRoadLenght + 5;j++)
             {
-                mvprintw(4,i,"_");
-                mvprintw(verticalRoadLenght+1,i,"_");
+                attron(COLOR_PAIR(ROAD_PAIR));
+                if((j<2 || j>verticalRoadLenght+4) || (i<2 || i > horizontalRoadLenght*2 + 7))
+                    attron(COLOR_PAIR(EDGE_PAIR));
+                if((i>9 && i < horizontalRoadLenght*2+4) && (j>4 && j < verticalRoadLenght+2))
+                    attron(COLOR_PAIR(GRASS_PAIR));
+                    
+                mvprintw(j,i,"-");
             }
         }
-        for(int i = 2; i <= verticalRoadLenght + 4;i++)
-        {
-            mvprintw(i,0,"|");
-            mvprintw(i,horizontalRoadLenght+6,"|");
-            
-            if(i>4 && i <= verticalRoadLenght+1)
-            {
-                mvprintw(i,7,"|");
-                mvprintw(i,horizontalRoadLenght-1,"|");
-            }
-        }
-
+       
         // pętla wyświetlająca samochody
+        attron(COLOR_PAIR(CAR_PAIR));
         for(int i = 0; i<cars.size();i++)
         {
             if(cars[i]->getIsDriving())
             {
                 string s = std::to_string(cars[i]->getId());
                 char const *pchar = s.c_str();
-                mvprintw(cars[i]->getPosY()+3,cars[i]->getPosX()+3,pchar);
+                mvprintw(cars[i]->getPosY()+3,cars[i]->getPosX()*2+5,pchar);
             }
             else
             {
                 cars.erase(cars.begin()+i);
             }
         }
+        attroff(COLOR_PAIR(CAR_PAIR));
         refresh();
         int ch = getch();
 
@@ -197,23 +248,25 @@ void draw(vector<Car*> &cars)
     cout<<"Zamykanie wszystkich watkow..."<<endl;
 }
 
+// Main
 int main()
 {
     srand(time(0));
     vector<Car*> carsVector;
+    vector<Car*> carsOnRightEdgeVector;
     vector<thread> carThreads;
-    carsVector.emplace_back(new Car(10,0,0));
+    carsVector.emplace_back(new Car(10,0,0,ref(carsOnRightEdgeVector)));
     carThreads.emplace_back(&Car::driveLaps,carsVector.back());
     thread ncursesThread(draw,ref(carsVector));
 
     int number = 0;
     while(isWorking)
     {
-        this_thread::sleep_for(chrono::milliseconds(rand()%10000));
+        this_thread::sleep_for(chrono::milliseconds(rand()%5000));
         number++;
-        if(number>9)
+        if(number>98)
             number = 0;
-        carsVector.emplace_back(new Car(10,0,number));
+        carsVector.emplace_back(new Car(10,0,number,ref(carsOnRightEdgeVector)));
         carThreads.emplace_back(&Car::driveLaps,carsVector.back());
     }
 
